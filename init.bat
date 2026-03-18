e621eed7b85568016545fba5aa22918d5584a222
Suke2004 <lyricsofsongs96@gmail.com>
2026-03-18 08:37:37 +0530

    deployable first

--- init.bat
+++
@@ -1,7 +0,0 @@
-npx -y create-next-app@latest portfolio --ts --tailwind --eslint --app --src-dir --import-alias "@/*" --use-npm
-xcopy /E /I /H /Y portfolio .
-rmdir /S /Q portfolio
-npm install framer-motion clsx tailwind-merge
-mkdir public
-move sequence public\sequence
-echo DONE > done.txt
